13e0be6a5ec30ef81ce6bf25896430217851eec7
Saad <programmergkg@gmail.com>
2019-11-19 12:16:16 +0500

    Final Year Project

---
+++ classes.js
@@ -0,0 +1,9 @@
+const CLASSES ={
+  0: 'Actinic Keratoses (Solar Keratoses) or intraepithelial Carcinoma (Bowenâ€™s disease)',
+  1: 'Basal Cell Carcinoma',
+  2: 'Benign Keratosis',
+  3: 'Dermatofibroma',
+  4: 'Melanoma',
+  5: 'Melanocytic Nevi',
+  6: 'Vascular skin lesion'
+}
